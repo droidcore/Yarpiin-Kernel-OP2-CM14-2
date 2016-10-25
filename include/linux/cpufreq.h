3cd0e4715c51ebfa6dad30c0def57261ff0a1b62
yarpiin <yarpiin@gmail.com>
2016-10-25 19:17:07 +0100

    add Ironactive gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -483,6 +483,9 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 #endif
 
 /*********************************************************************
